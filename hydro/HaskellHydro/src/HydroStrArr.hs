a882346c4ad12b70e8535890b69c62f174441c12
Ryan Alameddine <rhalameddin@gmail.com>
2023-04-29 13:30:07 -0700

    moved to stack and completed basic (map) generation

--- hydro/HaskellHydro/src/HydroStrArr.hs
+++ hydro/HaskellHydro/src/HydroStrArr.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE GADTs #-}
+module HydroStrArr (generateOutput) where
 
 import Control.Applicative
 import Control.Category (Category)
 import qualified Control.Category ((.), id)
 import Control.Arrow
 import Data.Typeable
+import Text.Format
 
 import qualified Data.Map as Map    
 
@@ -56,7 +58,7 @@ instance Category Channel where
     
 instance Arrow Channel where
     arr = CArr
-    --o = source_iter(vec![f])
+    --o = map(f)    NOT THIS: --o = source_iter(vec![f])
 
     --first (Channel a) = Channel (first a) 
 
@@ -66,9 +68,16 @@ instance Arrow Channel where
     -- a -> enumerate() -> [0]o
     -- b -> enumerate() -> [1]o
 
+    -- THIS IS JUST TO ADD A TAG, OTHERWISE COPIED FROM SOURCE
+    f &&& g = arrT "|x| (x, x)" (\b -> (b,b)) >>> f *** g
+
+--HELPERS TO ADD TAGS
+arrT    t = CTag t . arr
+liftA2T t f a b = CTag t (liftA2 f a b)
+
 
 select2 :: Channel i Bool -> Channel i (o, o) -> Channel i o
-select2 p c = CTag "selector" $ liftA2 selector p c
+select2 = liftA2T "|(b, (o1, o2))| b ? o1 : o2" selector
     where
         selector True  (o, _) = o
         selector False (_, o) = o
@@ -77,26 +86,18 @@ select2 p c = CTag "selector" $ liftA2 selector p c
 --diamond :: (i -> Bool) -> Channel i o -> Channel i o -> Channel i o
 
 
-cDouble  = CTag "*2" $ arr ((*2) :: Int -> Int)
-cPlusOne = CTag "+1" $ arr ((+1) :: Int -> Int)
+cDouble  = arrT "|x| x * 2" ((*2) :: Int -> Int)
+cPlusOne = arrT "|x| x + 1" ((+1) :: Int -> Int)
 
-cPlusOnePlusDouble = CTag "+" $ liftA2 (+) cPlusOne cDouble
+cPlusOnePlusDouble = liftA2T "|(x, y)| x + y" (+) cPlusOne cDouble
 
 --double if even, plusOne if odd
-cDoubleOrPlus = select2 (CTag "even" $ arr even) c
-    where c = CTag "(,)" $ liftA2 (,) cDouble cPlusOne
+cDoubleOrPlus = select2 (arrT "|x| x % 2 == 0" even) c
+    where c = liftA2T "|(x, y)| (x, y)" (,) cDouble cPlusOne
 
 -- for example, 5 will output (6, 12)
 cFancy = cDoubleOrPlus &&& (cDoubleOrPlus >>> cDoubleOrPlus)
---         CMap    :: (b -> c) -> Channel a b                      -> Channel a c
---         CPure   :: b                                            -> Channel a b
---         CLift   :: (b -> c -> d) -> Channel a b -> Channel a c  -> Channel a d
---         CId     ::                                                 Channel a a
---         CComp   :: Channel b c -> Channel a b                   -> Channel a c
---         CArr    :: (a -> b)                                     -> Channel a b
---         CMerge  :: Channel a b -> Channel a' b'                 -> Channel (a, a') (b, b')
 
--- m = Map.fromList [(t2, "a"), (p1, "b")]
 
 
 
@@ -112,6 +113,38 @@ instance Show (Channel a b) where
             s t CId            = "[CId]"
             s _ (CTag t c)     = s t c
 
+-- input: channel number of input to channel, i: counter for naming purposes, tag: function in Rust, c: channel to transform to rust
+--returns: (next i, rust string)
+--o = i -> map(f)
+toRust :: Int -> String -> Channel a b -> (Int, String)
+toRust i tag (CMap  f c)   = (cI, cStr ++ format "c{0} = c{1} -> map({2});\n" [show i, show (i + 1), tag])
+    where (cI, cStr) = toRust (i + 1) "" c
+--o = map(|x| constant)
+toRust i tag (CPure b)     = (i + 1, format "c{0} = map(|_| CONSTANT);\n" [show i])
+--o = cross_join() -> map(|(a, b)| f(a, b))
+--a -> [0]o
+--b -> [1]o
+toRust i tag (CLift f a b) = (bI, format "c{0} = cross_join() -> map({3});\nc{1} -> [0]c{0};\nc{2} -> [1]c{0};\n" [show i, show $ i + 1, show aI, tag])
+    where
+        (aI, aStr) = toRust (i + 1) "" a
+        (bI, bStr) = toRust aI "" b
+--o = a -> b
+toRust i tag (CComp b a)   = (bI, format "c{0} = c{1} -> c{2};\n" [show i, show aI, show $ i + 1])
+    where
+        (aI, aStr) = toRust (i + 1) "" a
+        (bI, bStr) = toRust aI "" b
+--o = map(f)
+toRust i tag (CArr  f)     = (i + 1, format "c{0} = map({1});\n" [show i, tag])
+-- o = join()
+-- a -> enumerate() -> [0]o
+-- b -> enumerate() -> [1]o
+toRust i tag (CMerge a b)  = (bI, format "c{0} = join();\nc{1} -> enumerate() -> [0]c{0};\nc{2} -> enumerate() -> [1]c{0};\n" [show i, show aI, show $ i + 1])
+    where
+        (aI, aStr) = toRust (i + 1) "" a
+        (bI, bStr) = toRust aI "" b
+--o = map(|x| x)
+toRust i tag CId           = (i + 1, format "c{0} = map(|x| x);\n" [show i]);
+toRust i _   (CTag t c)    = toRust i t c
 
 -- unwrap Nothing = ""
 -- unwrap (Just s) = s
@@ -119,4 +152,9 @@ instance Show (Channel a b) where
 --main = print $ show cDouble -- "[CArr, f: *2]"
 --main = print $ show cDoubleOrPlus -- "[CLift f:selector, a:[CArr, f: even], b:[CLift f:(,), a:[CArr, f: *2], b:[CArr, f: +1]]]"
 
-main = print $ show cFancy
+output = cDouble
+generateOutput = putStr rust
+    where 
+        (i, raw) = toRust 0 "" output
+        rust = format "\n{1}\ninput -> c0 -> output;" ["c" ++ show i, raw]
+--generateOutput = print $ show cFancy
---
+++ hydro/HaskellHydro/src/Lib.hs
@@ -0,0 +1,8 @@
+module Lib
+    ( someFunc
+    ) where
+
+import HydroStrArr
+
+someFunc :: IO ()
+someFunc = generateOutput
