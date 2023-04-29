4a0cd146e79f7bb362ab95fc47fd8e5bcfac170f
Ryan Alameddine <rhalameddin@gmail.com>
2023-04-28 20:16:15 -0700

    compared to relevant hydroflow code

--- hydromonad.hs
+++ hydromonad.hs
@@ -27,22 +27,43 @@ newtype Channel a b = Channel (a -> b)
 
 instance Functor (Channel a) where
     fmap f (Channel c) = Channel (fmap f c) --map over result
+    --o = i -> map(f)
 
 instance Applicative (Channel a) where
     pure = Channel . pure  --channel that throws away it's input and returns whatever is passed into pure
+    --o = i -> map(|x| constant)
+
+
     -- a <*> b :: Channel a (b -> c) -> Channel a b -> Channel a c
 
     -- (a1 -> b -> c) -> Channel a a1 -> Channel a b -> Channel a c
     -- f <$> a :: a -> (b -> c), b :: a -> b, f <$> a <*> b :: a -> c
     liftA2 f (Channel a) (Channel b) = Channel (liftA2 f a b)  --Channel (f <$> a <*> b) --merge the result of two channels with a cross join, then apply f
+    --o = cross_join() -> map(|(a, b)| f(a, b))
+    --a -> [0]o
+    --b -> [1]o
 
 instance Category Channel where
     id = Channel id
+    --o = map(|x| x)
+
     (Channel a) . (Channel b) = Channel (a . b)
+    --o = a -> b
     
 instance Arrow Channel where
     arr = Channel
-    first (Channel a) = Channel (first a) 
+    --o = source_iter(vec![f])
+    --note: I have to figure out how to send closures lol
+
+    --first (Channel a) = Channel (first a) 
+
+    (Channel a) *** (Channel b) = Channel (a *** b)
+    -- o = join()
+    -- a -> enumerate() -> [0]o
+    -- b -> enumerate() -> [1]o
+
+
+
 
 select2 :: (i -> Bool) -> Channel i (o, o) -> Channel i o
 select2 p = liftA2 selector (arr p)
@@ -63,7 +84,22 @@ cPlusOnePlusDouble = liftA2 (+) cPlusOne cDouble
 cDoubleOrPlus = select2 even c
     where c = liftA2 (,) cDouble cPlusOne
 
-cFinal = cDoubleOrPlus &&& (cDoubleOrPlus >>> cDoubleOrPlus)
+-- for example, 5 will output (6, 12)
+cFancy = cDoubleOrPlus &&& (cDoubleOrPlus >>> cDoubleOrPlus)
+
+
+-- data Message = Message Int Body
+
+-- data Body = Echo Int
+--             | Error String
+
+-- cEchoServer :: Channel Message Message
+-- cEchoServer = f 
+
+
+
+cFinal = cFancy
+
 
 --main :: IO ()
 main = interact (unlines . map process . lines)
@@ -71,6 +107,8 @@ main = interact (unlines . map process . lines)
         (Channel f) = cFinal
         process = show . f . read
 
+
+-- INITIAL TESTS
 -- main :: IO ()
 -- main = (print . show) o
 --     where
