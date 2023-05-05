8de05df80b63b82c605ed7c4412fd75ee19765be
Ryan Alameddine <rhalameddin@gmail.com>
2023-05-05 15:24:55 -0700

    organized

--- hydro/HaskellHydro/src/HydroArrow.hs
+++ hydro/HaskellHydro/src/HydroArrow.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE GADTs #-}
-module HydroArrow where
+module HydroArrow  where
 
 import Control.Applicative
 import Control.Category (Category)
@@ -74,81 +74,3 @@ cSelect2 = liftA2T "|(b, (o1, o2))| if b {o1} else {o2}" selector
         selector False (_, o) = o
 
 
-
-
-
-
-
-
--- RUST GENERATOR --
--- channel -> (hydroflow, input )
-rustGen :: Channel i o -> String
-rustGen c = format "\n{0}\ninput -> c0;\nc{1} -> output;" [rust, show o]
-    where (rust, o, _) = toRust 0 "" c
-
--- input #, tag, channel -> (HydroFlow string, output #, next free)
-toRust :: Int -> String -> Channel i o -> (String, Int, Int)
---o = i -> map(f)
---inputC = inputA
---outputC = outputA -> map(f)
-toRust inputC tag (CMap  f a) = (strA ++ strC, outputC, nextFree)
-    where
-        outputC = succ inputC
-        inputA = succ outputC
-        (strA, outputA, nextFree) = toRust inputA "" a
-        strC = format "c{0} = c{1};\nc{2} = c{3} -> map({4});\n" $ map show [inputC, inputA, outputC, outputA] ++ [tag]
---o = map(|x| constant)
---inputOutputC = map(|x| constant)
-toRust inOutC tag (CPure b) = (strC, inOutC, succ inOutC)
-    where
-        strC = format "c{0} = map(|x| {1});\n" [show inOutC, tag]
---o = cross_join() -> map(|(a, b)| f(a, b)); a -> [0]o; b -> [1]o
---inputC = tee()
---inputC[0] -> inputA
---inputC[1] -> inputB
---outputC = cross_join<'tick, 'tick>() -> map(f)
---outputA -> [0]outputC
---outputB -> [1]outputC
-toRust inputC tag (CLift f a b) = (strA ++ strB ++ strC, outputC, nextFree)
-    where
-        outputC = succ inputC
-        inputA = succ outputC
-        (strA, outputA, inputB)   = toRust inputA "" a
-        (strB, outputB, nextFree) = toRust inputB "" b
-        strC = format "c{0} = tee();\nc{0}[0] -> c{1};\nc{0}[1] -> c{2};\nc{3} = cross_join::<'tick, 'tick>() -> map({6});\nc{4} -> [0]c{3};\nc{5} -> [1]c{3};\n" $ map show [inputC, inputA, inputB, outputC, outputA, outputB] ++ [tag]
---o = a -> b
---inputC = inputA
---outputA -> inputB
---outputC = outputB
-toRust inputC tag (CComp b a) = (strA ++ strB ++ strC, outputC, nextFree)
-    where
-        outputC = succ inputC
-        inputA = succ outputC
-        (strA, outputA, inputB)   = toRust inputA "" a
-        (strB, outputB, nextFree) = toRust inputB "" b
-        strC = format "c{0} = c{1};\nc{4} -> c{2};\nc{3} = c{5};\n" $ map show [inputC, inputA, inputB, outputC, outputA, outputB] ++ [tag]
---o = map(f)
---inputOutputC = map(f)
-toRust inOutC tag (CArr f) = (strC, inOutC, succ inOutC)
-    where
-        strC = format "c{0} = map({1});\n" [show inOutC, tag]
--- o = join();a -> enumerate() -> [0]o;b -> enumerate() -> [1]o
---inputC = tee()
---inputC[0] -> map(|(x, _)| x) -> inputA
---inputC[1] -> map(|(_, x)| x) -> inputB
---outputC = join<'tick, 'tick>() -> map(|(k, v)| v);
---outputA -> enumerate() -> [0]outputC;
---outputB -> enumerate() -> [1]outputC;
-toRust inputC tag (CMerge b a) = (strA ++ strB ++ strC, outputC, nextFree)
-    where
-        outputC = succ inputC
-        inputA = succ outputC
-        (strA, outputA, inputB)   = toRust inputA "" a
-        (strB, outputB, nextFree) = toRust inputB "" b
-        strC = format "c{0} = tee();\nc{0}[0] -> map(|(x, _)| x) -> c{1};\nc{0}[1] -> map(|(_, x)| x) -> c{2};\nc{3} = join::<'tick, 'tick>() -> map(|(k, v)| v);\nc{4} -> enumerate() -> [0]c{3};\nc{5} -> enumerate() -> [1]c{3};\n" $ map show [inputC, inputA, inputB, outputC, outputA, outputB] ++ [tag]
---o = map(|x| x)
-toRust inOutC tag (CPure b) = (strC, inOutC, succ inOutC)
-    where
-        strC = format "c{0} = map(|x| x);\n" [show inOutC, tag]
---tag
-toRust i _ (CTag t c)    = toRust i t c
--- hydro/HaskellHydro/src/Lib.hs
+++ hydro/HaskellHydro/src/Lib.hs
@@ -1,31 +1,50 @@
 module Lib
-    ( someFunc
+    ( generateOutput
     ) where
 
 import HydroArrow
 import RustGenerator
+import Control.Arrow
+
+--Note: Although the haskell functions are provided next to their rust equivalents
+--this is simply to ensure that Haskell will type check the Channels for me.
 
 
---basic channels
+--cEven is a channel that takes in ints, and outputs bools (if the input is even)
+cEven :: Channel Int Bool
 cEven = arrT "|x| x % 2 == 0" even
-cAdd  = arrT "|(x, y)| x + y" (+)
+--Note that the this is accomplished by "lifting" the haskell even function into the Channel arrow
+--and then "tagging" it with the rust code, hence arrT instead of arr.
 
 cDouble  = arrT "|x| x * 2" (*2)
 cPlusOne = arrT "|x| x + 1" (+1)
 
+--cAdd is a channel that takes an input of type a, splits it across two
+--input channels, and combines the output with a (+) operation
+cAdd :: Channel a Int -> Channel a Int -> Channel a Int
+cAdd  = liftA2T "|(x, y)| x + y" (+)
+
+
 cPlusOnePlusDouble = cPlusOne `cAdd` cDouble
 
 
---double if even, plusOne if odd
+--cDoubleOrPlus doubles the input if even, adds 1 if odd
+cDoubleOrPlus :: Channel Int Int
 cDoubleOrPlus = cIf cEven cDouble cPlusOne
 
--- for example, 5 will output (6, 12)
+--Using arrow operators, cFancy takes an input, passes it into (cDoubleOrPlus) and (cDoubleOrPlus >>> cDoubleOrPlus).
+--The right hand side computes computes cDoubleOrPlus twice in a pipeline.
+--Finally, it takes the output of the two sides and joins them into a tuple.
+--For example, inputting 5 will output (6, 12)
 cFancy = cDoubleOrPlus &&& (cDoubleOrPlus >>> cDoubleOrPlus)
 
-cTest =  liftA2T "|(x, y)| x + y" (+) cPlusOne cDouble `CComp` cDouble
+--cTest takes an input and splits it into cPlusOne and cDouble.
+--It then takes those outputs and adds them together before piping that into cDouble.
+cTest =  cAdd cPlusOne cDouble >>> cDouble
 
 
-output :: Channel Int (Int, Int)
+--If you would like to test your own channels, simply set output = your_channel and run the program with `stack run`
 output = cFancy
+
 
 generateOutput = putStr $ rustGen output
